1c8d962ba8e480a22b0ec7db5d26e976f784a859
Markus Mäkelä <markus.makela@mariadb.com>
2019-04-12 12:59:12 +0300

    MXS-2349: Add socket support for maxctrl
    A server can now be created with a socket.

--- maxctrl/test/createdestroy.js
+++ maxctrl/test/createdestroy.js
@@ -104,6 +104,12 @@ describe("Create/Destroy Commands", function() {
             .should.be.rejected
     })
 
+    it('create and destroy server with socket', function() {
+        return verifyCommand('create server server5 /tmp/server.sock', 'servers/server5')
+            .then(() => doCommand('destroy server server5'))
+            .should.be.fulfilled
+    })
+
     it('create listener', function() {
         return verifyCommand('create listener RW-Split-Router my-listener 4567',
                             'services/RW-Split-Router/listeners/my-listener')
